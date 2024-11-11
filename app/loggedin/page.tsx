17611cddc13d9cae714ae4da0c080e9c6e805358
Dan Peterson <dmpeterson317@gmail.com>
2024-11-10 19:28:55 -0500

    hook up google oauth to front end

--- app/loggedin/page.tsx
+++ app/loggedin/page.tsx
@@ -1,15 +1,14 @@
-import { signInAction } from "@/actions/auth-action";
+import Tasks from "@/components/ui/apa/Tasks";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
       <main className="flex flex-col items-center justify-between p-24">
-        <h1>Login</h1>
+        <h1>apa</h1>
+
+        <Tasks />
 
 
-        <form action={signInAction}>
-          <Button>Log in</Button>
-        </form>
       </main>
 
   );
--- auth.ts
+++ auth.ts
@@ -1,7 +1,13 @@
 import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     //google oauth
+    GoogleProvider({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+      authorization: { params: { prompt: "consent" }},
+    }),
   ],
 })
