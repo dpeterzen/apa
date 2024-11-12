61d816c9c58121c083df2bad15c6b5c165f72c5e
Dan Peterson <dmpeterson317@gmail.com>
2024-11-11 21:56:17 -0500

    sync logged in user to db, stable state

--- app/loggedin/layout.tsx
+++ app/loggedin/layout.tsx
@@ -1,4 +1,4 @@
-import { Providers } from "../providers";
+import Providers from "../providers";
 import { auth } from "@/auth";
 
 export default async function RootLayout({
--- app/loggedin/page.tsx
+++ app/loggedin/page.tsx
@@ -1,5 +1,4 @@
 import Tasks from "@/components/ui/apa/Tasks";
-import { Button } from "@/components/ui/button";
 import UserProfile from "@/components/ui/apa/UserProfile";
 
 export default function Home() {
@@ -8,8 +7,6 @@ export default function Home() {
         <h1>apa</h1>
         <UserProfile />
         <Tasks />
-
-
       </main>
 
   );
