7c6339173580eae4532af03d1aa96fbf9b021436
Dan Peterson <dmpeterson317@gmail.com>
2024-12-21 20:54:35 -0500

    refactor: enforce onPositionChange prop as required and adjust button layout

--- components/tiles/tile-actions.tsx
+++ components/tiles/tile-actions.tsx
@@ -17,7 +17,7 @@ import { ReactNode } from "react";
 
 interface TileActionsProps {
   onSizeChange: (direction: "increase" | "decrease") => void;
-  onPositionChange?: (direction: "increase" | "decrease") => void;
+  onPositionChange: (direction: "increase" | "decrease") => void;
   onDelete: () => void;
   children?: ReactNode; // For popover content
   size: TileSize;
@@ -53,35 +53,31 @@ export function TileActions({
         </PopoverContent>
       </Popover>
 
-      {onPositionChange && (
-        <>
-          <Button
-            variant="ghost"
-            className="z-100 rounded-full absolute top-[28px] right-[-1px] h-6 w-6 p-0 text-muted"
-            onClick={() => onPositionChange("increase")}
-          >
-            <ChevronRight />
-          </Button>
-          <Button
-            variant="ghost"
-            className="z-100 rounded-full absolute top-[57px] right-[-1px] h-6 w-6 p-0 text-muted"
-            onClick={() => onPositionChange("decrease")}
-          >
-            <ChevronLeft />
-          </Button>
-        </>
-      )}
-
+      <Button
+        variant="ghost"
+        className="z-100 rounded-full absolute bottom-[28px] right-[-1px] h-6 w-6 p-0 text-muted"
+        onClick={() => onPositionChange("increase")}
+      >
+        <ChevronRight />
+      </Button>
       <Button
         variant="ghost"
         className="z-100 rounded-full absolute bottom-[-1px] right-[-1px] h-6 w-6 p-0 text-muted"
+        onClick={() => onPositionChange("decrease")}
+      >
+        <ChevronLeft />
+      </Button>
+
+      <Button
+        variant="ghost"
+        className="z-100 rounded-full absolute top-[57px] right-[-1px] h-6 w-6 p-0 text-muted"
         onClick={() => onSizeChange("increase")}
       >
         <Plus />
       </Button>
       <Button
         variant="ghost"
-        className="z-100 rounded-full absolute bottom-[28px] right-[-1px] h-6 w-6 p-0 text-muted"
+        className="z-100 rounded-full absolute top-[28px] right-[-1px] h-6 w-6 p-0 text-muted"
         onClick={() => onSizeChange("decrease")}
       >
         <Minus />
