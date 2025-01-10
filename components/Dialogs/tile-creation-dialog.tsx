377cf0153d47b985b79fb70f68cb447f5cef12d7
Dan Peterson <dmpeterson317@gmail.com>
2025-01-10 15:29:49 -0500

    replace create tile dialog with blank tile workflow

--- components/Dialogs/tile-creation-dialog.tsx
+++ components/Dialogs/tile-creation-dialog.tsx
@@ -1,58 +1,62 @@
-import { useState } from "react"
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
-import { Button } from "@/components/ui/button"
-import { WallSelector } from "../tiles/wall-selector"
-import { TileTypeSelector } from "../tiles/tile-type-selector"
-import { Id } from "@/convex/_generated/dataModel"
-import { SquarePlusIcon } from "@/components/icons/square-plus"
+import { useState } from "react";
 import {
-  SidebarMenuButton,
-} from "@/components/ui/sidebar"
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { WallSelector } from "../tiles/wall-selector";
+import { TileTypeSelector } from "../tiles/tile-type-selector";
+import { Id } from "@/convex/_generated/dataModel";
+import { SquarePlusIcon } from "@/components/icons/square-plus";
+import { SidebarMenuButton } from "@/components/ui/sidebar";
 
-type TileType = "note"
+type TileType = "note";
 
 interface TileCreationDialogProps {
-  onCreateTile: (wallId: Id<"walls">, type: TileType) => void
+  onCreateTile: (wallId: Id<"walls">, type: TileType) => void;
 }
 
 export function TileCreationDialog({ onCreateTile }: TileCreationDialogProps) {
-  const [selectedWallId, setSelectedWallId] = useState<Id<"walls">>()
-  const [selectedType, setSelectedType] = useState<TileType>()
-  const [isOpen, setIsOpen] = useState(false)
+  const [selectedWallId, setSelectedWallId] = useState<Id<"walls">>();
+  const [selectedType, setSelectedType] = useState<TileType>();
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleCreate = () => {
     if (selectedWallId && selectedType) {
-      onCreateTile(selectedWallId, selectedType)
-      setIsOpen(false)
-      setSelectedWallId(undefined)
-      setSelectedType(undefined)
+      onCreateTile(selectedWallId, selectedType);
+      setIsOpen(false);
+      setSelectedWallId(undefined);
+      setSelectedType(undefined);
     }
-  }
+  };
 
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
       <DialogTrigger asChild>
-      <SidebarMenuButton>
+        <SidebarMenuButton>
           <span className="text-blue-500 font-semibold flex items-center">
             <SquarePlusIcon className="size-7 ml-[-2px] mr-1" />
             Add tile
           </span>
-          </SidebarMenuButton>
+        </SidebarMenuButton>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
           <DialogTitle>Add Tile</DialogTitle>
         </DialogHeader>
         <div className="space-y-4">
-          <WallSelector 
+          <WallSelector
             value={selectedWallId}
             onValueChange={setSelectedWallId}
           />
-          <TileTypeSelector 
+          <TileTypeSelector
             value={selectedType}
             onValueChange={setSelectedType}
           />
-          <Button 
+          <Button
             onClick={handleCreate}
             disabled={!selectedWallId || !selectedType}
             className="w-full"
@@ -62,5 +66,5 @@ export function TileCreationDialog({ onCreateTile }: TileCreationDialogProps) {
         </div>
       </DialogContent>
     </Dialog>
-  )
+  );
 }
