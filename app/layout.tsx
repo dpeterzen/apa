134aa25d0f208b64f458e81c00bd2537a63b16f2
Dan Peterson <dmpeterson317@gmail.com>
2025-01-26 20:45:18 -0500

    name change p2

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "TileRecall",
+  title: "TidyRecall",
   description: "An Open Source AI-Powered Productivity Tool",
   icons: {
     icon: [
