ca61b1196a284a6a569b6491683b99953b6516ec
Dan Peterson <dmpeterson317@gmail.com>
2025-01-26 20:45:03 -0500

    change app name back to original name

--- components/nav/nav-title.tsx
+++ components/nav/nav-title.tsx
@@ -29,7 +29,7 @@ export function NavTitle() {
       <SidebarMenuItem>
       <div className="flex gap-1 py-2 px-1">
         <LayoutDashboardIcon className={`size-6`} />
-        <h1 className="font-semibold text-[16px]">TileRecall</h1>
+        <h1 className="font-semibold text-[16px]">TidyRecall</h1>
       </div>
 
       </SidebarMenuItem>
